f03392ee3c72745e81c7538b82ad14ea7192cded
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2025-04-12 18:30:39 -0600

    Otp stuff (#17)
    * add OTP stuff and refernces
    
    * clean up code

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -4,6 +4,7 @@ from src.gui.styles import PROGRAM_FONT
 from src.gui.characters_tool import component_2
 from src.gui.encryption_decryption_tool import component_3
 from src.gui.caesar_cipher_tool import component_4
+from src.gui.otp_tool import component_1
 
 
 def create_main_window():
@@ -14,17 +15,6 @@ def create_main_window():
     # Right content frame that will be updated
     content_frame = tk.Frame(root, bg="white")
     content_frame.pack(side="right", fill="both", expand=True)
-
-    # === Define your 4 components as functions ===
-    # These can be defined in another file and then called in each of these function
-    def component_1(parent):
-        label = tk.Label(
-            parent,
-            text="This is OTP hex <-> binary",
-            font=(PROGRAM_FONT, 18),
-            bg="white",
-        )
-        label.pack(expand=True)
 
     components = (component_1, component_2, component_3, component_4)
 
